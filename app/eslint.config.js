4e58849df95e79ea3f823267b1f001a1d845f172
leticia sousa <88945495+thzlet@users.noreply.github.com>
2026-04-17 18:06:06 -0300

    Enhance ESLint config with comments for better understanding
    Updated ESLint configuration to include comments for clarity.

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,15 +1,16 @@
+// configuração do ESLint no formato “flat config”
 import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals' // variaveis globais
+import reactHooks from 'eslint-plugin-react-hooks' // validando regras de hooks
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from 'typescript-eslint' // suporte ao ts
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
+    files: ['**/*.{ts,tsx}'], // aplica regras so em ts e tsx
+    extends: [ // extensoes
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
